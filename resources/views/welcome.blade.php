4cd797e0d8043797cf39a327be7660dc706ea221
ngtinn59 <108003591+ngtinn59@users.noreply.github.com>
2023-03-09 21:08:26 +0700

    ttin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -540,41 +540,10 @@
             </div>
         </div>
     </section>
-     <!-- Partner Logo Section Begin -->
-    <div class="partner-logo">
-        <div class="container">
-            <div class="logo-carousel owl-carousel">
-                <div class="logo-item">
-                    <div class="tablecell-inner">
-                        <img src="./front/img/logo-carousel/logo-1.png" alt="">
-                    </div>
-                </div>
-                <div class="logo-item">
-                    <div class="tablecell-inner">
-                        <img src="./front/img/logo-carousel/logo-2.png" alt="">
-                    </div>
-                </div>
-                <div class="logo-item">
-                    <div class="tablecell-inner">
-                        <img src="./front/img/logo-carousel/logo-3.png" alt="">
-                    </div>
-                </div>
-                <div class="logo-item">
-                    <div class="tablecell-inner">
-                        <img src="./front/img/logo-carousel/logo-4.png" alt="">
-                    </div>
-                </div>
-                <div class="logo-item">
-                    <div class="tablecell-inner">
-                        <img src="./front/img/logo-carousel/logo-5.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Instagram Section begin -->
-    <div class="instagram-photo">
+    
+    
+     <!-- Instagram Section begin -->
+     <div class="instagram-photo">
         <div class="insta-item set-bg" data-setbg="./front/img/insta-1.jpg">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
@@ -612,6 +581,8 @@
             </div>
         </div>
     </div>
+    <!-- Instargram Section End -->
+
     <!-- Last about-project Section Begin -->
     <section class="lastest-about-project spad">
         <div class="container">
@@ -655,8 +626,38 @@
             </div>
         </div>
     </section>
-     <!-- Partner Logo Section End -->
-
+    <!-- Partner Logo Section Begin -->
+    <div class="partner-logo">
+        <div class="container">
+            <div class="logo-carousel owl-carousel">
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="./front/img/logo-carousel/logo-1.png" alt="">
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="./front/img/logo-carousel/logo-2.png" alt="">
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="./front/img/logo-carousel/logo-3.png" alt="">
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="./front/img/logo-carousel/logo-4.png" alt="">
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="./front/img/logo-carousel/logo-5.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
      <!-- Footer Section Begion -->
      <footer class="footer-section">
         <div class="container">
